e264b80db3344756d093ee8ec1566484bd35d825
SeongMin <hsm950825@naver.com>
2018-12-17 15:41:15 +0900

    movie&movieposter구현

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Movie from './Movie';
 
 class App extends Component {
   render() {
+
+    const movieTitles = [
+      "Matrix",
+      "Full Metal Jacket",
+      "Oldboy",
+      "Star wars",
+    ]
+    const movieImage = [
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg",
+      "https://media.comicbook.com/2018/06/star-wars-darth-vader-yoda-comicbookcom-1116397-1280x0.jpeg",
+    ]
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Movie title={movieTitles[0]} poster={movieImage[0]}/>
+        <Movie title={movieTitles[1]} poster={movieImage[1]}/>
+        <Movie title={movieTitles[2]} poster={movieImage[2]}/>
+        <Movie title={movieTitles[3]} poster={movieImage[3]}/>
       </div>
+
     );
   }
 }
---
+++ src/Movie.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Movie.css';
+
+class Movie extends Component{
+    render(){
+        return(
+            <div>
+                <MoviePoster poster={this.props.poster} />
+                <h1>{this.props.title}</h1>
+            </div>    
+        )
+    }
+}
+
+class MoviePoster extends Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.poster} />
+            </div>
+        )
+    }
+}
+
+export default Movie;
