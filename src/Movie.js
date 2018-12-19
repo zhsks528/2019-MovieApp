a2a1006bbd0e428660421575fe4ae310fac2677a
SeongMin <hsm950825@naver.com>
2018-12-19 15:43:53 +0900

    component를 function으로 구현

--- src/Movie.js
+++ src/Movie.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react';
+import React, { component } from 'react';
 import './Movie.css';
+import PropTypes from 'prop-types';
 
-class Movie extends Component{
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>    
-        )
-    }
+// class Movie extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <MoviePoster poster={this.props.poster} />
+//                 <h1>{this.props.title}</h1>
+//             </div>    
+//         )
+//     }
+// }
+
+function Movie({title, poster}){
+    return(
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component{
-    render(){
-        return(
-            <div>
-                <img src={this.props.poster} />
-            </div>
-        )
-    }
+Movie.propTypes = {
+    title : PropTypes.string.isRequird,
+    poster : PropTypes.string.isRequird,
+}
+// class MoviePoster extends Component{
+//     render(){
+//         return(
+//             <div>
+//                 <img src={this.props.poster} />
+//             </div>
+//         )
+//     }
+// }
+
+function MoviePoster({poster}){
+    return (
+        <img src={poster} alt= "Movie Poster"/>
+    )
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
 }
 
 export default Movie;
