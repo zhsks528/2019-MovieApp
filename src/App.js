477191869c97245e5cb68b08d2369dbceecc3764
SeongMin <hsm950825@naver.com>
2018-12-19 15:42:32 +0900

    _renderMovies()함수를 구현 & Loading 구현

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import Movie from './Movie';
 
 class App extends Component {
-  render() {
 
-    const movieTitles = [
-      "Matrix",
-      "Full Metal Jacket",
-      "Oldboy",
-      "Star wars",
-    ]
-    const movieImage = [
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
-      "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg",
-      "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg",
-      "https://media.comicbook.com/2018/06/star-wars-darth-vader-yoda-comicbookcom-1116397-1280x0.jpeg",
-    ]
+  //Render : componentWillmount() -> render() -> componentDidMount()
+
+  //update : componentWillReceiveProps() -> shouldComponentUpdate() -> componetWillUpdate() -> render() -> componentDidUpdate()
+
+  state = {
+  }
+
+  componentDidMount(){
+    setTimeout(()=> {
+      this.setState({
+        movies : [
+          {
+            title: "Matrix",
+            poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+          },
+          {
+            title: "Full Metal Jacket",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg",
+          },
+          {
+            title: "OldBoy",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg",
+          },
+          {
+            title: "Star wars",
+            poster: "https://media.comicbook.com/2018/06/star-wars-darth-vader-yoda-comicbookcom-1116397-1280x0.jpeg"
+          }
+        ]
+      })
+    }, 5000)
+  }
+
+  _renderMoives = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })
+
+    return movies;
+
+  //_ 쓰는이유 : React 함수와 내 함수를 구분하기 위해서..
+  }
+  render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImage[0]}/>
-        <Movie title={movieTitles[1]} poster={movieImage[1]}/>
-        <Movie title={movieTitles[2]} poster={movieImage[2]}/>
-        <Movie title={movieTitles[3]} poster={movieImage[3]}/>
+        {this.state.movies ? this._renderMoives() : "Loading"}
       </div>
 
     );
