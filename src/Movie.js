1723bdce2ed0e86af7c6da02436939f19ec9996a
zhsks528 <hsm950825@naver.com>
2019-01-02 10:52:02 +0900

    Movie_App MovieGenre 함수 추가 / Movie_App CSS 추가

--- src/Movie.js
+++ src/Movie.js
@@ -1,4 +1,4 @@
-import React, { component } from 'react';
+import React from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types';
 
@@ -13,11 +13,21 @@ import PropTypes from 'prop-types';
 //     }
 // }
 
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis}){
     return(
-        <div>
-            <MoviePoster poster={poster} />
-            <h1>{title}</h1>
+        <div className="Movie">
+            <div className="Movie__Column">
+                <MoviePoster poster={poster} alt={title}/>
+            </div>
+            <div className="Movie__Column">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre = {genre} key={index} />)}
+                </div>
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+            </div>
         </div>
     )
 }
@@ -25,6 +35,8 @@ function Movie({title, poster}){
 Movie.propTypes = {
     title : PropTypes.string.isRequird,
     poster : PropTypes.string.isRequird,
+    genres : PropTypes.array.isRequired,
+    synopsis : PropTypes.string.isRequired,
 }
 // class MoviePoster extends Component{
 //     render(){
@@ -36,14 +48,25 @@ Movie.propTypes = {
 //     }
 // }
 
-function MoviePoster({poster}){
+function MoviePoster({poster, alt}){
     return (
-        <img src={poster} alt= "Movie Poster"/>
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
     )
 }
 
 MoviePoster.propTypes = {
     poster: PropTypes.string.isRequired,
+    alt : PropTypes.string.isRequired,
+}
+
+function MovieGenre({genre}){
+    return(
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
+MovieGenre.propTypes = {
+    genre : PropTypes.string.isRequired,
 }
 
 export default Movie;
